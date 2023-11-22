41e291fe6c15d7289c9436549f26702dd14e0fd5
edujav <edujav21@gmail.com>
2023-11-22 20:24:19 -0300

    added env and django sentry

--- prueba_tecnica/celery.py
+++ prueba_tecnica/celery.py
@@ -3,12 +3,11 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-# Define el nombre de tu aplicación Django y establece la configuración de Celery
+#configuracion celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prueba_tecnica.settings')
 app = Celery('prueba_tecnica')
 
-# Lee la configuración de Django para Celery
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-# Descubre automáticamente tareas en la aplicación Django
+# obtiene todas las tareas
 app.autodiscover_tasks()
--- prueba_tecnica/settings.py
+++ prueba_tecnica/settings.py
@@ -9,9 +9,18 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import sentry_sdk
+import environ
 
+from sentry_sdk.integrations.django import DjangoIntegration
 from pathlib import Path
 
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+# Read env file
+environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-fvoqgg(xt15(jw!41unbu3lu54^byw3la-mu*r*y+voii$_ge4"
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -30,7 +39,7 @@ ALLOWED_HOSTS = []
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://localhost:6379/1",  # Cambia esto según tu configuración de Redis
+        "LOCATION": env('REDIS_HOST'),  # Cambia esto según tu configuración de Redis
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
@@ -137,10 +146,28 @@ STATIC_URL = "static/"
 
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = env('CELERY_BROKER_URL')
+CELERY_RESULT_BACKEND = env('CELERY_RESULT_BACKEND')
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_TIMEZONE = 'UTC'
 
+sentry_sdk.init(
+    dsn=env('SENTRY_DSN'),
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+    integrations=[
+        DjangoIntegration(
+            transaction_style='url',
+            middleware_spans=True,
+            signals_spans=True,
+            cache_spans=True,
+        ),
+    ],
+)

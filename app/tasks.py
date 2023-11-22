6e8c3cd108f45438de66d3c7fb941e6f544ba910
edujav <edujav21@gmail.com>
2023-11-22 20:23:45 -0300

    minor fixes

--- app/tasks.py
+++ app/tasks.py
@@ -13,17 +13,20 @@ from app.models import Orders, OrdersDetail
 def save_orders_clients(compressedOrdersData):
     compressor = lzstring.LZString()
 
-    #En el BE descomprimir los datos con lzstring.
+    # En el BE descomprimir los datos con lzstring.
     orders_json = compressor.decompressFromBase64(compressedOrdersData)
 
-    #convertir el JSON a tipo de datos python.
+    # Convertir el JSON a tipo de datos python.
     client_orders = json.loads(orders_json)
     ultimo_pedido = Orders.objects.last()
-    numero_pedido = 1
+    numero_pedido = 0
     if ultimo_pedido is not None:
-        numero_pedido = ultimo_pedido.numero_pedido
+        numero_pedido = ultimo_pedido.pedido_numero
+
+    # Guardos los datos en la base de datos.
     with transaction.atomic():
         for client_id, client_order_items in client_orders.items():
+            numero_pedido += 1
             order = Orders(pedido_numero=numero_pedido,
                            pdvobj_id=client_id,
                            doc_tipo="doct_tipo",
@@ -33,18 +36,17 @@ def save_orders_clients(compressedOrdersData):
                 order_detail = OrdersDetail(**client_order)
                 order_detail.orderobj = order
                 order_detail.save()
-            numero_pedido += 1
 
 
 @shared_task
 def save_sales_data(data: dict):
-    #Comprimir el json con lzstring.
+    # Comprimir el json con lzstring.
     compressor = lzstring.LZString()
 
-    #Convertir a json la estructura.
+    # Convertir a json la estructura.
     data_json = json.dumps(data)
 
-    #Guarda la estructura comprimida en la base de datos redis
+    # Guarda la estructura comprimida en la base de datos redis
     compressed = compressor.compressToUTF16(data_json)
-
+    # Guarda en Redis
     cache.set("data", compressed)
